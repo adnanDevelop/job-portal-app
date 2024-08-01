3d10e4558bd703ec04f67a4d64c79316d3ab2b55
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-08-01 19:47:42 +0500

    fixed update api bug

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  updateProfile,
+} from "../controllers/userController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = Router();
 
 router.route("/user/register").post(register);
 router.route("/user/login").post(login);
-router.route("/user/profile/update").post(isAuthenticated, logout);
+router.route("/user/profile/update").post(isAuthenticated, updateProfile);
 
 export default router;
