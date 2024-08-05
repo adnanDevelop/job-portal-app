67d65db84046521c2a57a07ebc0bb5ec08d96c93
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-08-05 18:45:27 +0500

    completed service page

--- frontend/src/modules/service/index.tsx
+++ frontend/src/modules/service/index.tsx
@@ -1,5 +1,21 @@
+import JobFeature from "../about/component/JobFeature";
+import PageHeader from "../../components/global/PageHeader";
+import ExploreJob from "../home/component/ExploreJob";
+
 const Service = () => {
-  return <div>Service</div>;
+  return (
+    <main>
+      {/* Page header section */}
+      <section>
+        <PageHeader title="How it works?" breadCrumb="Services" />
+      </section>
+
+      <section className="padding-inline">
+        <JobFeature />
+        <ExploreJob />
+      </section>
+    </main>
+  );
 };
 
 export default Service;
