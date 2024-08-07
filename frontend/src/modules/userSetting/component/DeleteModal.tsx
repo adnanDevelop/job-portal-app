ec21b8a1bda734dfc61503562c83b310bf8d3b0d
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-08-07 19:51:13 +0500

    added delete modal in user setting

--- frontend/src/modules/userSetting/component/DeleteModal.tsx
+++ frontend/src/modules/userSetting/component/DeleteModal.tsx
@@ -12,26 +12,26 @@ const DeleteModal = ({ id }: { id: string }) => {
 
   return (
     <Modal id={id}>
-      <div className="modal-box w-[400px] text-center">
+      <div className="modal-box w-[400px] text-center bg-light-blue">
         <div className="w-[70px] h-[70px] mx-auto rounded-full grid place-items-center bg-red-100">
           <GoAlertFill className="text-[30px] text-red-500" />
         </div>
-        <h3 className="text-lg font-bold text-center text-[30px] text-heading-color mt-4">
+        <h3 className=" font-bold text-center text-[25px] text-white mt-4">
           Delete Account
         </h3>
-        <p className="mt-3 leading-5 text-content-color">
+        <p className="mt-3 leading-5 text-white">
           You&apos;re going to delete your account. Are you sure?
         </p>
         <div className="flex items-center justify-center w-full modal-action">
           <button
-            className="px-[30px] h-[40px] rounded-lg bg-gray-200 text-content-color font-medium transitions hover:scale-105 text-sm"
+            className="px-[30px] h-[40px] rounded-lg bg-gray-200 text-black font-medium font-jakarta transitions hover:scale-105 text-sm"
             onClick={closeModal}
           >
             No, Keep it.
           </button>
           <button
             type="submit"
-            className="px-[30px] h-[40px] rounded-lg bg-red-500 text-white font-medium transitions hover:scale-105 text-sm"
+            className="px-[30px] h-[40px] rounded-lg bg-red-500 text-white font-jakarta font-medium transitions hover:scale-105 text-sm"
           >
             Yes, Delete it!
           </button>
